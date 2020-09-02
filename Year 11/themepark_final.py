ff0715a3c7f1c7170ccef70e61eaa29ed99bdcae
haydenso <sohayden929@gmail.com>
2020-09-02 10:31:27 +0800

    added calculation functions

--- Year 11/themepark_final.py
+++ Year 11/themepark_final.py
@@ -1,5 +1,15 @@
-class Overall:
+import math
 
+def test_int(n):
+    try:
+        int(n)
+        pass
+        return int(n)
+    except (ValueError, SyntaxError):
+        raise Exception("Invalid input")
+
+class Overall:
+    #TASK 1
     def __init__(self):
         self.num_students = 0
         self.ticket_price = 30
@@ -7,44 +17,77 @@ class Overall:
         self.max_students = 45
         self.paid_students = []
         self.unpaid_students = []
+        self.num_freeticket = 0
 
+    #TASK 1
     def inputNumStudents(self):
         while True:
-            self.num_students = int(input("How many are estimated to go? "))
-            if self.num_students > 45:
-                print("Maximum of 45 participants")
+            try:
+                self.num_students = int(input("How many are estimated to go? "))
+                test_int(self.num_students)
+                if self.num_students > 45:
+                    print("Maximum of 45 participants")
+                    continue
+                else:
+                    break
+            except Exception:
+                print("Invalid input")
                 continue
-            else:
-                break
         return self.num_students
 
-    def calcCPC(self, freeticket=None):  #=None makes the parameter optional
-        self.cost_per_student = self.coach_price/self.num_students + self.ticket_price
+    #TASK 1
+    def calcCPC(self):  #CPC = Cost Per Student
+        self.freeticket(self.num_students)
+        self.cost_per_student = math.ceil((self.coach_price + ((self.num_students - self.num_freeticket) * self.ticket_price)) / self.num_students) #math.ceil to round up all the time
+        print(f"The current cost per student is ${self.cost_per_student}")
+        return self.cost_per_student
 
-    def freeticket(self, num_students):
-        self.num_freeticket = int(round(num_students/10))
-        print(self.num_freeticket)
-        return self.num_freeticket
+    #TASK 1
+    def freeticket(self, n):
+        try:
+            self.num_freeticket = round(n/10)
+            return self.num_freeticket
+        except:
+            print("No free tickets")  #returns the default value of 0
 
-    def add_paid_student(self, name):
-        if int(len(self.paid_students) + 1) <= 45:
-            self.paid_student.append(name)
-        return self.paid_student
-            
-    def __str__(self):   #does all the calculations of prices 
-        self.num_students_paid = len(self.paid_students) + 1
-        self.freeticket(self.num_students_paid) # calculates number of free tickers
+    #TASK 2
+    def add_student(self, name, list_name):
+        if int(len(list_name) + 1) <= 45:
+            list_name.append(name)
+        else:
+            print("Maximum number of students")
+        return list_name
+
+    #TASK 3
+    def calc_price(self):
+        self.num_paid = len(self.paid_students) 
+        self.freeticket(self.num_paid) # calculates number of free tickets
         self.discount = self.num_freeticket * self.ticket_price 
-        self.total_cost = int(self.num_students_paid) * int(self.ticket_price) + self.coach_price - self.discount
-        return f" {self.num_students} said they are going to the trip\n {self.num_students_paid} have paid\n You get {self.num_freeticket} free tickers\n Total cost is ${self.total_cost} (minus the cost of free tickets)"
+        self.total_cost = int(self.num_paid) * int(self.ticket_price) + self.coach_price - self.discount
+        return self.total_cost
 
-#input_error = "Invalid input"
+    #TASK 3  
+    def __str__(self):   #does all the calculations of prices   
+        self.calc_price()
+        return f" {self.num_students} said they are going to the trip\n {self.num_paid} have paid\n You get {self.num_freeticket} free ticket\n Total cost is ${self.total_cost} (minus the cost of free tickets)"
 
-# try:
-#     print("")
-# except Exception as input_error:
-#     pass
 
 trip_class = Overall()
 trip_class.inputNumStudents()
 
+trip_class.add_student("John", trip_class.paid_students)
+trip_class.add_student("Hayden", trip_class.paid_students)
+trip_class.add_student("Hayden", trip_class.paid_students)
+trip_class.add_student("Hayden", trip_class.paid_students)
+trip_class.add_student("Hayden", trip_class.paid_students)
+trip_class.add_student("Hayden", trip_class.paid_students)
+trip_class.add_student("Hayden", trip_class.paid_students)
+trip_class.add_student("Hayden", trip_class.paid_students)
+trip_class.add_student("Hayden", trip_class.paid_students)
+trip_class.add_student("Hayden", trip_class.paid_students)
+trip_class.add_student("Hayden", trip_class.paid_students)
+
+trip_class.add_student("Kiran", trip_class.unpaid_students)
+
+trip_class.calcCPC()
+print(trip_class)
