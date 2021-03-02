e58f7cb1cca0252e68465beebec7f4fdfaae2f29
haydenso <sohayden929@gmail.com>
2021-03-02 23:32:54 +0800

    First draft commit

--- Pre-release/first_draft.py
+++ Pre-release/first_draft.py
@@ -1,95 +1,109 @@
+# TASK 1 - Start of Day 
+# status default value is "open" and is update to "Closed" when steats == tally
+up_times = ["9:00", "11:00", "13:00", "15:00"]
+up_seats = [480, 480, 480, 480]
+up_tally = [0, 0, 0, 0]
+up_revenue = [0, 0, 0, 0]
+up_status = ["open", "open", "open", "open"]
+
+down_times = ["10:00", "12:00", "14:00", "16:00"]
+down_seats = [480, 480, 480, 640]
+down_tally = [0, 0, 0, 0]
+down_revenue = [0, 0, 0, 0]
+down_status = ["open", "open", "open", "open"]
+
 ticket_cost = 25
+# $25 each journey because when we go up we might have enough people for a free ticket, however we come down different times
 
-# status default value is "open" and is update to "closed" when total_steats==count
-journey_up_times = ["9:00", "11:00", "13:00", "15:00"]
-journey_up_seats = [480, 480, 480, 480]
-journey_up_count = [0, 0, 0, 0]
-journey_up_revenue = [0, 0, 0, 0]
-journey_up_status = ["open", "open", "open", "open"]
-
-journey_down_times = ["10:00", "12:00", "14:00", "16:00"]
-journey_down_seats = [480, 480, 480, 640]
-journey_down_count = [0, 0, 0, 0]
-journey_down_revenue = [0, 0, 0, 0]
-journey_down_status = ["open", "open", "open", "open"]
-
-# the reason they are $25 each journey up and down is because when we go up we might have enough people for a free ticket, however we might come down different times, hence 
-# Start of the day
-def task_1():
-
-    print("\nJourneys Up")
+def screen_display(times, seats):
     for i in range(0,4):
-        print(f"{i+1}. {journey_up_times[i]} - {journey_up_seats[i]} seats")
+        print(f"{i+1}. {times[i]} - {seats[i]} seats")
     
-    print("\nJourneys Down")
-    for i in range(0,4):
-        print(f"{i+1}. {journey_down_times[i]} - {journey_down_seats[i]} seats")
+print("\n Journey Up")
+screen_display(up_times, up_seats)
+print("\n Journey Down")
+screen_display(down_times, down_seats)
 
-# Purchasing tickets
-def task_2():
+# TASK 2 - Purchasing tickets
+def check_seats(times, status, tally, session):
+        if status == "Closed":
+            print(f"{times} journey seat allocated")
+            tally += 1
+            session += 1
+            return True
+        else:
+            print(f"Sorry {times} full already")
+            return False
+
+def calc_revenue(session, revenue):
+    for i in range(0,4):
+        num_free_tickets = int(session[i]) // 10
+        num_tickets_to_buy = session[i] - num_free_tickets
+        # $25 being the ticket
+        cost = num_tickets_to_buy * ticket_cost
+        up_revenue[i] += cost
+
+# While there are still spaces for going up 
+while up_status != ["Closed", "Closed", "Closed", "Closed"]:
+    # session running totals for finding number of free tickets, it resets back to 0 after each session closes
+    up_session = [0, 0, 0, 0] 
+    down_session = [0, 0, 0, 0]
+
+    # Ask for how many passengers 1-80 inclusive
     while True:
-        num = int(input("Number of students going:"))
+        num = int(input("Number of students going: "))
         if 1 <= num <= 80:
             break
         else:
             print("Invalid number of tickets (Must be between 1 and 80 inclusive)")
-    
-    while True:
-        for i in range (1,num+1):
+
+    # Ask individually each passenger
+    for i in range(1,num+1):
+        while True:
             # Ask for the journey number as listed from task 1 (e.g input 1 is 9:00)
-            time_up = input(f"Which journey down is passenger {i} going on?")
-            time_down = input(f"Which journey down is passenger {i} going on?")
+            time_up = int(input(f"Which journey down is passenger {i} going on? "))
+            time_down = int(input(f"Which journey down is passenger {i} going on? "))
 
-            while journey_up_status[time_up-1] != "Closed":
-                journey_up_count[time_up-1] += 1
-                journey_up_revenue[time_up+1] += ticket_cost
+            if check_seats(up_times[time_up-1], up_status[time_up-1], up_tally[time_up-1], up_session[time_up-1]) and check_seats(down_times[time_down-1], down_status[time_up-1], down_tally[time_up-1], down_session[time_up-1]):
                 break
-            while journey_down_status[time_down-1] != "Closed":
-                journey_down_count[time_down-1] += 1
-                journey_down_revenue[time_down+1] += ticket_cost
-                break
+            else:
+                continue
 
-            for i in range(0,4):
-                if journey_up_count[i] == journey_up_seats:
-                    journey_up_status[i] = "Closed"
-                if journey_down_count[i] == journey_down_seats:
-                    journey_down_status[i] = "Closed"
+        for i in range(0,4):
+            if up_tally[i] == up_seats:
+                up_status[i] = "Closed"
+            if down_tally[i] == down_seats:
+                down_status[i] = "Closed"
 
-    # Testing if it is still open for 
+    # The reason there is a need for the up_session and down_session lists are because the tally is the running total; 
+    # There will be multiple sessions each day hence it is inaccurate to find number of free tickets based on the tally 
+    calc_revenue(up_session, up_revenue)
+    calc_revenue(down_session, down_revenue)
+    session_total_cost = sum(up_revenue) + sum(down_revenue)
+    print(f"This session's total is ${session_total_cost}")
 
-    num_free_tickets = int(num) // 10
-    num_tickets_to_buy = num - num_free_tickets
-    total_cost = num_tickets_to_buy * ticket_cost
-    print(f"This session's total is ${total_cost}")
+# TASK 3
+def task_3_display(times, tally, revenue):
+    for i in range(0,4):
+        print(f"{i+1}. {times[i]} - {tally[i]} passengers - ${revenue[i]} made")
+        highest = max(tally)
+        return highest
 
-# End of the day
-def task_3():
-    print("\nJourneys Up")
-    for i in range(1,5):
-        print(f"{i}. {journey_up_times[i-1]} - {journey_up_count[i-1]} passengers - ${journey_up_revenue[i-1]} made")
-    
-    print("\nJourneys Down")
-    for i in range(1,5):
-        print(f"{i}. {journey_down_times[i-1]} - {journey_down_count[i-1]} passengers - ${journey_down_revenue[i-1]} made")
+print("\nJourney Up")
+up_highest = task_3_display(up_times, up_tally, up_revenue)
+print("\nJourney Down")
+down_highest = task_3_display(down_times, down_tally, down_revenue)
 
-    total_revenue = sum(journey_up_revenue) + sum(journey_down_revenue)
-    total_passengers = sum(journey_up_count) + sum(journey_down_count)
-    # Two lists are combined
-    highest_passenger_up = max(journey_up_count)
-    highest_passenger_down = max(journey_down_count)
+total_revenue = sum(up_revenue) + sum(down_revenue)
+print(f"\nA total of ${total_revenue} has been made")
+total_passengers = sum(up_tally) + sum(down_tally)
+print(f"There are {total_passengers} passengers today")
 
-    if highest_passenger_up > highest_passenger_down:
-        highest_passenger = highest_passenger_up
-    elif highest_passenger_up < highest_passenger_down:
-        highest_passenger = highest_passenger_down
-    else:
-        highest_passenger = [highest_passenger_up, highest_passenger_down]
-
-    print(f"\nA total of ${total_revenue} has been made")
-    print(f"There are {total_passengers} passengers today")
-    print(f"This journey had the most {highest_passenger}")
-
-task_1()
-task_3()
-
-# What if two journey have the same number of passenger count?
+# What if two journey have the same number of passenger tally, how do we determine the max?
+highest_passenger = max(up_highest, down_highest)
+if highest_passenger == max(up_tally):
+    index = up_tally.index(highest_passenger)
+    print(f"{up_times[index]} was the journey with most passengers")
+elif highest_passenger == max(down_tally):
+    index = up_tally.index(highest_passenger)
+    print(f"{down_times[index]} was the journey with most passengers")
